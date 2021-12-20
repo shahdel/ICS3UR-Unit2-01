b6dc3775cd160731a55161a77f681c71b0a51ae1
EC2 Default User <ec2-user@ip-172-31-45-129.ec2.internal>
2021-12-20 17:33:54 +0000

    added area_and_perimeter.py

---  area_and_perimeter.py
+++  area_and_perimeter.py
@@ -1,1 +1,23 @@
+#!/usr/bin/env python3
 
+# Created by: Shahd Elsaman
+# Created on November 2021
+# This program calculates the area and perimeter of a circle
+#    with radius 15mm
+
+import math
+
+
+def main():
+    # this function calculates the area and perimeter
+
+    print("If a circle has a radius of 15mm:")
+    print("")
+    print("Area is {} mm².".format(math.pi * 15 ** 2))
+    print("perimeter is {} mm.".format(2 * math.pi * 15))
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
